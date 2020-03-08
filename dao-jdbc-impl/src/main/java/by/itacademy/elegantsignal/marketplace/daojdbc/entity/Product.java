5bd033c9b7936595fa91d8f0df2f44db74dbcf8e
elegantsignal <elegantsignal@gmail.com>
2020-03-08 20:04:19 +0300

    minor fix

--- dao-jdbc-impl/src/main/java/by/itacademy/elegantsignal/marketplace/daojdbc/entity/Product.java
+++ dao-jdbc-impl/src/main/java/by/itacademy/elegantsignal/marketplace/daojdbc/entity/Product.java
@@ -16,42 +16,52 @@ public class Product extends BaseEntity implements IProduct {
 	private Date created;
 	private Date updated;
 
+	@Override
 	public IUserAccount getUserAccount() {
 		return userAccount;
 	}
 
+	@Override
 	public void setUserAccount(IUserAccount userAccount) {
 		this.userAccount = userAccount;
 	}
 
+	@Override
 	public ProductType getType() {
 		return type;
 	}
 
+	@Override
 	public void setType(ProductType type) {
 		this.type = type;
 	}
 
+	@Override
 	public BigDecimal getPrice() {
 		return price;
 	}
 
+	@Override
 	public void setPrice(BigDecimal price) {
 		this.price = price;
 	}
 
+	@Override
 	public Date getCreated() {
 		return created;
 	}
 
+	@Override
 	public void setCreated(Date created) {
 		this.created = created;
 	}
 
+	@Override
 	public Date getUpdated() {
 		return updated;
 	}
 
+	@Override
 	public void setUpdated(Date updated) {
 		this.updated = updated;
 	}
