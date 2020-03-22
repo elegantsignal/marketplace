7424d9107130ad64fdb4d4d31fadefe43329873c
elegantsignal <elegantsignal@gmail.com>
2020-03-22 14:37:16 +0300

    mix BaseEntity_ (wrong cleanup parameters)

--- dao-orm-impl/src/main/java/by/itacademy/elegantsignal/marketplace/dao/orm/impl/UserDaoImpl.java
+++ dao-orm-impl/src/main/java/by/itacademy/elegantsignal/marketplace/dao/orm/impl/UserDaoImpl.java
@@ -13,7 +13,6 @@ import javax.persistence.metamodel.SingularAttribute;
 import org.hibernate.jpa.criteria.OrderImpl;
 import org.springframework.stereotype.Repository;
 
-import by.itacademy.elegantsignal.marketplace.dao.orm.impl.entity.BaseEntity_;
 import by.itacademy.elegantsignal.marketplace.dao.orm.impl.entity.User;
 import by.itacademy.elegantsignal.marketplace.dao.orm.impl.entity.User_;
 import by.itacademy.elegantsignal.marketplace.daoapi.IUserDao;
@@ -65,12 +64,12 @@ public class UserDaoImpl extends AbstractDaoImpl<IUser, Integer> implements IUse
 
 	private SingularAttribute<? super User, ?> toMetamodelFormat(final String sortColumn) {
 		switch (sortColumn) {
-		case "id":
-			return BaseEntity_.id;
-		case "name":
-			return User_.name;
-		default:
-			throw new UnsupportedOperationException("sorting is not supported by column:" + sortColumn);
+			case "id":
+				return User_.id;
+			case "name":
+				return User_.name;
+			default:
+				throw new UnsupportedOperationException("sorting is not supported by column:" + sortColumn);
 		}
 	}
 
