00cc4387e048f8de3da95aa1cae9e88f7d1e9311
elegantsignal <elegantsignal@gmail.com>
2020-05-11 23:18:06 +0300

    add lombok
    squash me with add lombok

--- web/src/main/java/by/itacademy/elegantsignal/marketplace/web/dto/BookDTO.java
+++ web/src/main/java/by/itacademy/elegantsignal/marketplace/web/dto/BookDTO.java
@@ -1,116 +1,45 @@
 package by.itacademy.elegantsignal.marketplace.web.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.io.File;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.Set;
 
-import org.springframework.format.annotation.DateTimeFormat;
 
-
+@Getter
+@Setter
 public class BookDTO {
 
 	private Integer id;
-	private BigDecimal price;
-	private Set<Integer> genreIds;
+	private String author;
 	private String title;
-	private String cover;
-
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	private LocalDate published;
-
+	private Set<Integer> genreIds;
 	private String description;
+	private String cover;
+	private BigDecimal price;
+	@DateTimeFormat(pattern = "yyyy-MM-dd") private LocalDate published;
 	private Date created;
 	private Date updated;
-	private String author;
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(final String title) {
-		this.title = title;
-	}
-
-	public String getCover() {
-		return cover;
-	}
-
-	public void setCover(final File cover) {
-		if (cover != null) {
-			this.cover = cover.toString();
-			return;
-		}
-		this.cover = "";
-	}
 
 	public void setCover(final String cover) {
 		this.cover = cover;
 	}
 
-	public LocalDate getPublished() {
-		return published;
+	public void setCover(final File cover) {
+		if (cover != null) {
+			setCover(cover.toString());
+			return;
+		}
+		setCover("");
 	}
 
 	public void setPublished(final LocalDate localDate) {
 		this.published = localDate;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(final String description) {
-		this.description = description;
-	}
-
-	public Date getCreated() {
-		return created;
-	}
-
-	public void setCreated(final Date created) {
-		this.created = created;
-	}
-
-	public Date getUpdated() {
-		return updated;
-	}
-
-	public void setUpdated(final Date updated) {
-		this.updated = updated;
-	}
-
-	public void setId(final Integer id) {
-		this.id = id;
-	}
-
-	public Integer getId() {
-		return this.id;
-	}
-
-	public BigDecimal getPrice() {
-		return price;
-	}
-
-	public void setPrice(final BigDecimal price) {
-		this.price = price;
-	}
-
-	public Set<Integer> getGenreIds() {
-		return genreIds;
-	}
-
-	public void setGenreIds(final Set<Integer> genreIds) {
-		this.genreIds = genreIds;
-	}
-
-	public void setAuthor(final String author) {
-		this.author = author;
-	}
-
-	public String getAuthor() {
-		return author;
 	}
 
 }
